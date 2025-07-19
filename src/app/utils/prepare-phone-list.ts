51f27df7cac19b8b4134be7668eed8fd77d05ae2
Wesley Marques <wesleymqsss@gmail.com>
2025-07-19 19:40:19 -0300

    tratamento de erro dos inputs de contato e adicionado tipagem no parametro da funcao => prepare-phone-list

--- src/app/utils/prepare-phone-list.ts
+++ src/app/utils/prepare-phone-list.ts
@@ -1,9 +1,10 @@
 import { PhoneTypeEnum } from "../enums/phone-type.enum";
+import { IphoneToDisplay } from "../interfaces/iphone-to-display.interface";
 import { IPhone } from "../interfaces/user/phone.interface";
 import { PhoneList } from "../types/phone-list";
 import { PhoneTypeDescriptionMap } from "./phone-type-description-map";
 
-export const preparePhoneList = (originalUserPhoneList: PhoneList, isDisplayPhone: boolean, callback: (phone: { type: number; typeDescription: string; phoneNumber: string; }) => void) => {
+export const preparePhoneList = (originalUserPhoneList: PhoneList, isDisplayPhone: boolean, callback: (phone: IphoneToDisplay) => void) => {
     Object.keys(PhoneTypeDescriptionMap).map(Number).forEach((phoneType: number) => {
         const phoneFound = originalUserPhoneList?.find((userPhone: IPhone) => userPhone.type === phoneType);
         let phoneNumber = '';
